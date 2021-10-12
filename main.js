79741421ae29e9c1dac924721ca6cb2fce54d53f
deyso <bauer.dezso@gmail.com>
2021-10-13 00:15:41 +0200

    test

---
+++ main.js
@@ -0,0 +1,16 @@
+let test = async () => {
+  let url = "https://git.heroku.com/deytdl.git/download";
+  let data = { URL: "https://www.youtube.com/watch?v=oJiyK0UNx_0" };
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  console.log("hello");
+  return response.json(); // parses JSON response into native JavaScript objects
+};
